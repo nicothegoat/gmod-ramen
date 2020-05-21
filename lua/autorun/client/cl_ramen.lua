e19a96d15cc4bc5262edd1abf89f8801d21b5901
NicoTheGoat <minecrafter2010@gmail.com>
2020-05-21 18:08:02 +0200

    Don't poll draw distance cvar for changes

--- lua/autorun/client/cl_ramen.lua
+++ lua/autorun/client/cl_ramen.lua
@@ -8,21 +8,28 @@ local draw_SimpleText = draw.SimpleText
 local draw_GetFontHeight = draw.GetFontHeight
 local draw_SimpleTextOutlined = draw.SimpleTextOutlined
 
+
 local convarDrawDistance = CreateClientConVar("cl_ramen_drawdistance", "256",
 	true, false, "Set to 0 to disable cade ban text rendering.")
+
+local drawDistanceSquared = (convarDrawDistance:GetInt() or 256) ^ 2
+
+local function updateDrawDistance(convar, oldValue, newValue)
+	local drawDistance = tonumber(newValue)
+	if drawDistance then drawDistanceSquared = drawDistance ^ 2 end
+end
+
+cvars.AddChangeCallback("cl_ramen_drawdistance", updateDrawDistance)
+
 
 local markedPlayers = {}
 local textColor = Color(255, 0, 0, 255)
 
 local function hookHUDPaint()
+	if drawDistanceSquared == 0 then return end
+
 	local localPlayer = LocalPlayer()
-
 	if localPlayer:Team() ~= TEAM_HUMAN then return end
-
-	local maxDistance = convarDrawDistance:GetInt()
-	local maxDistanceSquared = maxDistance * maxDistance
-
-	if maxDistance == 0 then return end
 
 	local offset = 72
 
@@ -46,13 +53,13 @@ local function hookHUDPaint()
 		elseif IsValid(plr) then
 			local distanceSquared = localPlayer:GetPos():DistToSqr(plr:GetPos())
 
-			if distanceSquared < maxDistanceSquared then
+			if distanceSquared < drawDistanceSquared then
 				local worldPosition = plr:GetPos()
 				worldPosition.z = worldPosition.z + offset
 
 				local position = worldPosition:ToScreen()
 
-				textColor.a = (1 - distanceSquared / maxDistanceSquared) * textAlpha
+				textColor.a = (1 - distanceSquared / drawDistanceSquared) * textAlpha
 
 				draw_SimpleText(text, textFont, position.x, position.y, textColor,
 					TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
